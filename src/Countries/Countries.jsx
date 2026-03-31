b7dae3e51be810122114af895448cd73c154eda2
Md Royet <rroyet8@gmail.com>
2026-03-31 12:58:09 +0600

    State Props & Count

--- src/Countries/Countries.jsx
+++ src/Countries/Countries.jsx
@@ -1,16 +1,28 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import Country from "../Country/Country";
+import "./Countries.css";
 
 const Countries = ({ countriesPromise }) => {
+  const [visitedCountries, setVisitedCountries] = useState([]);
+
+  const handleVisitedCountries = (country) => {
+    console.log(country);
+  };
   const countriesData = use(countriesPromise);
   const countries = countriesData.countries;
 
   return (
     <div>
       <h1>Total Countries: {countries.length}</h1>
-      {countries.map((country) => (
-        <Country key={countries.id} country={country}></Country>
-      ))}
+      <div className="countries">
+        {countries.map((country) => (
+          <Country
+            key={countries.id}
+            country={country}
+            handleVisitedCountries={handleVisitedCountries}
+          ></Country>
+        ))}
+      </div>
     </div>
   );
 };
--- src/Country/Country.jsx
+++ src/Country/Country.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Country.css";
 
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountries }) => {
+  const [visited, setVisited] = useState(false);
+
+  const handleVisited = () => {
+    setVisited(visited ? false : true);
+    handleVisitedCountries(country);
+  };
   return (
-    <div>
+    <div className={`country ${visited && "Country-Visited"}`}>
       <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
       <h3>Name: {country.name.common}</h3>
       <p>Population: {country.population.population}</p>
+      <p>
+        Area: {country.area.area}{" "}
+        {country.area.area > 300000 ? "Big Countries" : "Small Countries"}{" "}
+      </p>{" "}
+      <br /> <br />
+      <button className="radiant-btn" onClick={handleVisited}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
     </div>
   );
 };
