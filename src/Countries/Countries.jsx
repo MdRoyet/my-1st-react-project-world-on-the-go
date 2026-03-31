c7625afe9ed99a346aaa92f8f3f7fed09cc4fc33
Md Royet <rroyet8@gmail.com>
2026-03-31 11:44:52 +0600

    Flags Displayed

--- src/Countries/Countries.jsx
+++ src/Countries/Countries.jsx
@@ -1,12 +1,16 @@
 import React, { use } from "react";
+import Country from "../Country/Country";
 
 const Countries = ({ countriesPromise }) => {
   const countriesData = use(countriesPromise);
   const countries = countriesData.countries;
-  console.log(countries);
+
   return (
     <div>
       <h1>Total Countries: {countries.length}</h1>
+      {countries.map((country) => (
+        <Country key={countries.id} country={country}></Country>
+      ))}
     </div>
   );
 };
